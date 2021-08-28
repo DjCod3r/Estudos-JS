bd20cbe0477a282192daaa447eb8f78203f008a5
DjCod3r <Djonatanschvambach@gmail.com>
2021-08-27 22:53:12 -0300

    Update refatorandoCallbackHell.js

--- ESNext/refatorandoCallbackHell.js
+++ ESNext/refatorandoCallbackHell.js
@@ -2,23 +2,29 @@ const http = require('http');
 
 const getTurma = (letra, callback) => {
     const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
-    http.get(url,res=> {
-        let resultado = ''
-        res.on('data', dados =>{
-            resultado += dados
-        })
-        res.on('end', () =>{
-            callback(JSON.parse(resultado))
+    return new Promise((resolve, reject) =>{
+        http.get(url,res=> {
+            let resultado = ''
+            res.on('data', dados =>{
+                resultado += dados
+            })
+            res.on('end', () =>{
+               try{
+                   resolve(JSON.parse(resultado))
+               }catch(e){
+                   reject(e)
+               }
+            })
         })
     })
 }
 
 
 let nomes =[]
-getTurma('A', alunos => {
+getTurma('A').then(alunos => {
     console.log(alunos[0])
     nomes = nomes.concat(alunos.map(a => `A: ${a.nome}`))
-  getTurma('B', alunos =>{
+  getTurma('B').then(alunos =>{
     nomes = nomes.concat(alunos.map(a => `B ${a.nome}`))
         console.log(nomes)
 })
